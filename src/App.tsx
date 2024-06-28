9d8ea9dc55163dae6679398274f4cf4f7f55862a
Daniel24356 <igwedaniel128@gmail.com>
2024-06-27 17:26:12 -0700

    Description of changes

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Course from './components/Course'
+import Footer from './components/footer'
+
+function App() {
+
+  return (
+    <>
+    <Course/>
+     <Footer/>
+    </>
+  )
+}
+
+export default App
